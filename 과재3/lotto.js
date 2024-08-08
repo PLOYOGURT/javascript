05533d236ce6380bed9cf39a937507374cb7fb9c
My-desiny-ae <tonnyk1130@gmail.com>
2024-08-08 14:38:54 +0900

    REFACTOR :: 과제3 fill, map, foreach 함수 사용, ADD :: 과제3 css 추가

--- 과재3/lotto.js
+++ 과재3/lotto.js
@@ -1,5 +1,7 @@
 
-// const submit = document.querySelector(".random"); 
+
+const submit = document.querySelector(".random");
+const reset = document.querySelector(".reset");
 
 // const num1 = document.getElementById('num1');
 // const num2 = document.getElementById('num2');
@@ -8,48 +10,34 @@
 // const num5 = document.getElementById('num5');
 // const num6 = document.getElementById('num6');
 
-// const numarr = [num1, num2, num3, num4, num5, num6]; 
+// const numarr = [num1, num2, num3, num4, num5, num6];
+const numarr = Array.from({ length: 6 }, (_, i) => document.getElementById(`num${i + 1}`));
 
-// submit.addEventListener('click', function(){
-//     const randNums = [];
-//     for (let i = 0; i < 6; i++) {
-//         let rand;
-//         do {
-//             rand = Math.floor(Math.random() * 45) + 1; 
-//         } while (randNums.includes(rand)); 
-//         randNums.push(rand);
-//     }
-//     for (let i = 0; i < 6; i++) {
-//         setTimeout(function(){
-//             numarr[i].textContent = randNums[i];
-//           }, 500*i);
-        
-//     }
-// });
-
-
-
-const submit = document.querySelector(".random");
-const reset = document.querySelector(".reset");
-
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const num3 = document.getElementById('num3');
-const num4 = document.getElementById('num4');
-const num5 = document.getElementById('num5');
-const num6 = document.getElementById('num6');
-
-const numarr = [num1, num2, num3, num4, num5, num6];
 
 submit.addEventListener('click', function(){
+
+    //기존 코드
+    // const randNums = [];
+    // for (let i = 0; i < 6; i++) {
+    //     let rand;
+    //     do {
+    //         rand = Math.floor(Math.random() * 45) + 1;
+    //     } while (randNums.includes(rand));
+    //     randNums.push(rand);
+    // }
+
+    //fill과 map 사용
     const randNums = [];
-    for (let i = 0; i < 6; i++) {
+    Array(6).fill().map(() => {
         let rand;
         do {
             rand = Math.floor(Math.random() * 45) + 1;
         } while (randNums.includes(rand));
         randNums.push(rand);
-    }
+    });
+    
+    
+    
 
     const updateCounter = (element, targetNum) => {
         const increment = targetNum / 100;
@@ -64,15 +52,31 @@ submit.addEventListener('click', function(){
         update();
     };
 
-    for (let i = 0; i < 6; i++) {
-        setTimeout(function(){
-            updateCounter(numarr[i], randNums[i], 500);
+
+//기존 코드
+    // for (let i = 0; i < 6; i++) {
+    //     setTimeout(function(){
+    //         updateCounter(numarr[i], randNums[i], 500);
+    //     }, 500 * i);
+    // }
+//foreach 함수 사용
+    randNums.forEach((num, i) => {
+        setTimeout(() => {
+            updateCounter(numarr[i], num);
         }, 500 * i);
-    }
+    });
 });
 
+
+//기존 코드
+// reset.addEventListener('click', function(){
+//     for(let i = 0; i<6; i++){
+//         numarr[i].textContent = 0;
+//     }
+// });
+
+
+//foreach함수 사용
 reset.addEventListener('click', function(){
-    for(let i = 0; i<6; i++){
-        numarr[i].textContent = 0;
-    }
+    numarr.forEach(num => num.textContent = 0);
 });
