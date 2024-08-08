0d2e89ea30ed1f39d5b8f03333d52aa5a551ea6f
My-desiny-ae <tonnyk1130@gmail.com>
2024-08-08 13:12:22 +0900

    ADD :: 과제3 카운트업,초기화 구현

--- 과재3/lotto.js
+++ 과재3/lotto.js
@@ -1,6 +1,5 @@
 
-
-// const submit = document.querySelector("random");
+// const submit = document.querySelector(".random"); 
 
 // const num1 = document.getElementById('num1');
 // const num2 = document.getElementById('num2');
@@ -9,20 +8,29 @@
 // const num5 = document.getElementById('num5');
 // const num6 = document.getElementById('num6');
 
-// const numarr = [num1, num2, num3, num4, num5, num6];
+// const numarr = [num1, num2, num3, num4, num5, num6]; 
 
 // submit.addEventListener('click', function(){
-//     const rand1 = Math.floor(Math.random() * 46);
-//     const rand2 = Math.floor(Math.random() * 46);
-//     const rand3 = Math.floor(Math.random() * 46);
-//     const rand4 = Math.floor(Math.random() * 46);
-//     const rand5 = Math.floor(Math.random() * 46);
-//     const rand6 = Math.floor(Math.random() * 46);
-//     for(let i = 1; i<=6; i++){
-//         numarr['num'+i].
+//     const randNums = [];
+//     for (let i = 0; i < 6; i++) {
+//         let rand;
+//         do {
+//             rand = Math.floor(Math.random() * 45) + 1; 
+//         } while (randNums.includes(rand)); 
+//         randNums.push(rand);
+//     }
+//     for (let i = 0; i < 6; i++) {
+//         setTimeout(function(){
+//             numarr[i].textContent = randNums[i];
+//           }, 500*i);
+        
 //     }
 // });
-const submit = document.querySelector(".random"); 
+
+
+
+const submit = document.querySelector(".random");
+const reset = document.querySelector(".reset");
 
 const num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
@@ -31,21 +39,40 @@ const num4 = document.getElementById('num4');
 const num5 = document.getElementById('num5');
 const num6 = document.getElementById('num6');
 
-const numarr = [num1, num2, num3, num4, num5, num6]; 
+const numarr = [num1, num2, num3, num4, num5, num6];
 
 submit.addEventListener('click', function(){
     const randNums = [];
     for (let i = 0; i < 6; i++) {
         let rand;
         do {
-            rand = Math.floor(Math.random() * 45) + 1; 
-        } while (randNums.includes(rand)); 
+            rand = Math.floor(Math.random() * 45) + 1;
+        } while (randNums.includes(rand));
         randNums.push(rand);
     }
+
+    const updateCounter = (element, targetNum) => {
+        const increment = targetNum / 100;
+        const update = () => {
+            const count = +element.textContent;
+            const nextCount = Math.ceil(count + increment);
+            element.textContent = nextCount > targetNum ? targetNum : nextCount;
+            if (count < targetNum) {
+                requestAnimationFrame(update);
+            }
+        };
+        update();
+    };
+
     for (let i = 0; i < 6; i++) {
         setTimeout(function(){
-            numarr[i].textContent = randNums[i];
-          }, 500*i);
-        
+            updateCounter(numarr[i], randNums[i], 500);
+        }, 500 * i);
+    }
+});
+
+reset.addEventListener('click', function(){
+    for(let i = 0; i<6; i++){
+        numarr[i].textContent = 0;
     }
 });
